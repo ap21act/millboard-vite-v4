943325c79454df6ea0f69c4b81aff91c1b81bd9d
Adarsha <paudeladarsha111@gmail.com>
2024-10-07 13:17:10 +0100

    second commit

--- frontend/src/Utils/getRandomData.js
+++ frontend/src/Utils/getRandomData.js
@@ -1,6 +1,6 @@
 // Utility function to shuffle and select random FAQs
 export const getRandomData = (data, count) => {
-    // Shuffle the array
+    // Shuffle the array in the 
     const shuffled = data.sort(() => 0.5 - Math.random());
     // Return the specified number of random elements
     return shuffled.slice(0, count);
