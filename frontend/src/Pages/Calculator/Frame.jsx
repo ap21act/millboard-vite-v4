171c179a31228ec94a8134d334c133581401e46c
Adarsha <paudeladarsha111@gmail.com>
2024-10-18 14:22:28 +0100

    calculator done except calculation

--- frontend/src/Pages/Calculator/Frame.jsx
+++ frontend/src/Pages/Calculator/Frame.jsx
@@ -4,16 +4,19 @@ const Frame = ({ imageSrc, labelText, size = 'w-48 h-48', textSize = 'text-2xl',
   return (
     <div
       onClick={onClick}
-      className={`border py-8 px-8 cursor-pointer transition-colors duration-300 hover:bg-grey flex flex-col items-start ${size} ${isSelected ? 'border-green bg-grey' : ''}`}
+      className={`border rounded-md p-4 cursor-pointer transition-all duration-300 hover:bg-gray-100 flex flex-col items-center justify-between ${size} ${isSelected ? 'border-green bg-grey' : 'bg-white'}`}
     >
       {/* Image */}
-      <img
-        src={imageSrc}
-        alt={labelText}
-        className="object-cover w-full h-full"
-      />
+      <div className="w-full h-full flex justify-center items-center mb-4">
+        <img
+          src={imageSrc}
+          alt={labelText}
+          className="object-contain w-full h-72"
+        />
+      </div>
+
       {/* Label Text */}
-      <label className={`font-semibold ${textSize} ${gap}`}>
+      <label className={`font-semibold ${textSize} text-center ${gap}`}>
         {labelText}
       </label>
     </div>
