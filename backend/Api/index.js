00dea806ee81d6937e2a3bd7764d706796f93c29
Adarsha <paudeladarsha111@gmail.com>
2024-10-29 15:30:46 +0000

    .

--- backend/Api/index.js
+++ backend/Api/index.js
@@ -2,18 +2,13 @@ import dotenv from 'dotenv';
 import app from '../app.js';
 import connectDB from '../Database/index.js';
 
-dotenv.config({
-    path: './.env'
-});
-const port=process.env.PORT || 5000;
-connectDB()
-.then(()=>{
-    app.listen(port,()=>{
-        console.log(`Server running on port ${port}`);
-    })
-})
-.catch((error)=>{
-    console.log("Server failed to start: ", error);
-})
+dotenv.config();
 
-
+export default async (req, res) => {
+    try {
+        await connectDB(); // Connect to the database
+        return app(req, res); // Use app as the serverless function handler
+    } catch (error) {
+        res.status(500).json({ message: "Server failed to start", error });
+    }
+};
