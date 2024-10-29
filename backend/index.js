6e121d22d79ed1ff375a3095a68857e5b2c337ca
Adarsha <paudeladarsha111@gmail.com>
2024-10-29 16:36:07 +0000

    .

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
-import app from '../app.js';
-import connectDB from '../Database/index.js';
+import app from './app.js';
+import connectDB from './Database/index.js';
 
 dotenv.config({
     path: './.env'
