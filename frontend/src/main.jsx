7d2f61164b9c5480ea893f062090d2d87ed05896
Adarsha <paudeladarsha111@gmail.com>
2024-10-30 08:33:19 +0000

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import './Styles/Main/index.css';
-import store from './redux/store'; // Import the Redux store
+import store from './Redux/store.js'; // Import the Redux store
 
 // Create the root element and wrap the app with Provider and BrowserRouter
 createRoot(document.getElementById('root')).render(
