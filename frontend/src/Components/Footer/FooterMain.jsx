102d359090ff80e47307e80e6243e02dc4323864
Adarsha <paudeladarsha111@gmail.com>
2024-10-30 15:33:45 +0000

    .

--- frontend/src/Components/Footer/FooterMain.jsx
+++ frontend/src/Components/Footer/FooterMain.jsx
@@ -1,89 +1,85 @@
 import React from 'react';
 import StayinTheLoop from '../Components/StayinTheLoop';
-import CustomLink from '../Components/Common/CustomLink'; 
+import CustomLink from '../Components/Common/CustomLink';
 
 const FooterMain = () => {
   return (
-    <div className=" mx-auto w-full max-w-screen-xl  hidden md:flex">
+    <div className=" mx-auto w-full max-w-screen-xl hidden md:flex">
       <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
         <div>
-        <div className='flex-col'>
-          <h2 className="mb-4 font-F37-light text-primary text-xl">Our Products</h2>
-          <ul className="  font-extrabold mb-5 text-sm">
-            Decking
-            <li className="text-xs font-semibold leading-8">
-              <CustomLink href="#">Millboard Collections</CustomLink>
-            </li>
-            <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Enhanced Grain</CustomLink>
-            </li>
-            <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Weathered Oak</CustomLink>
-            </li>
-            <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Lasta Grip</CustomLink>
-            </li>
-          </ul>
+          <div className='flex-col'>
+            <h2 className="mb-4 font-F37-light text-primary text-xl">Our Products</h2>
+            <ul className="font-extrabold mb-5 text-sm">
+              Decking
+              <li className="text-xs font-semibold leading-8">
+                <CustomLink to="/products/decking/collection">Millboard Collections</CustomLink>
+              </li>
+              <li className="text-xs leading-6 font-F37-light">
+                <CustomLink to="/products/decking/collection/enhanced-grain/antique-oak">Enhanced Grain</CustomLink>
+              </li>
+              <li className="text-xs leading-6 font-F37-light">
+                <CustomLink to="/products/decking/collection/weathered-oak/driftwood">Weathered Oak</CustomLink>
+              </li>
+              <li className="text-xs leading-6 font-F37-light">
+                <CustomLink to="/products/decking/collection/lasta-grip/coppered-oak">Lasta Grip</CustomLink>
+              </li>
+            </ul>
 
-          <ul className="  font-extrabold pb-2 leading-8  text-sm">
-            Cladding
-            <li className="text-xs leading-6 font-semibold">
-              <CustomLink href="#">Envello</CustomLink>
-            </li>
-            <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Board & Batten +</CustomLink>
-            </li>
-            <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Décor</CustomLink>
-            </li>
-            <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Shadow Line +</CustomLink>
-            </li>
-          </ul>
-        </div>
+            <ul className="font-extrabold pb-2 leading-8 text-sm">
+              Cladding
+              <li className="text-xs leading-6 font-semibold">
+                <CustomLink to="/products/cladding/collection">Envello</CustomLink>
+              </li>
+              <li className="text-xs leading-6 font-F37-light">
+                <CustomLink to="/products/cladding/collection/board-and-batten+/antique-oak">Board & Batten +</CustomLink>
+              </li>
+              <li className="text-xs leading-6 font-F37-light">
+                <CustomLink to="/products/cladding/collection/decor">Décor</CustomLink>
+              </li>
+              <li className="text-xs leading-6 font-F37-light">
+                <CustomLink to="/products/cladding/collection/shadow-line+/antique-oak">Shadow Line +</CustomLink>
+              </li>
+            </ul>
+          </div>
 
-        {/* Our Products Row 2 */}
+          {/* Our Products Row 2 */}
           <div>
             <h2 className="mb-4 font-F37-light text-primary text-xl">Inspiration</h2>
-            <ul className="  font-extrabold pb-2 text-sm">
-              
-            <li className="text-xs leading-6 font-F37-light">
-                  <CustomLink href="#">Blog</CustomLink>
-                </li>
-                <li className="text-xs leading-6 font-F37-light">
-                  <CustomLink href="#">Case Studies</CustomLink>
-                </li>
-                <li className="text-xs leading-6 font-F37-light">
-                  <CustomLink href="#">Gallery</CustomLink>
+            <ul className="font-extrabold pb-2 text-sm">
+              <li className="text-xs leading-6 font-F37-light">
+                <CustomLink to="/inspiration/articles">Blog</CustomLink>
+              </li>
+              <li className="text-xs leading-6 font-F37-light">
+                <CustomLink to="/case-studies/all">Case Studies</CustomLink>
+              </li>
+              <li className="text-xs leading-6 font-F37-light">
+                <CustomLink to="/inspiration-and-ideas/ideas/gallery">Gallery</CustomLink>
               </li>
             </ul>
           </div>
         </div>
-        
-        {/* Our Products Column 1 */}
-
 
         {/* Discover Millboard */}
         <div className="ml-5">
           <h2 className="mb-4 font-F37-light text-primary text-xl">Discover <br />Millboard</h2>
           <ul className="font-extrabold pb-2 text-sm">
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">About us</CustomLink>
+              <CustomLink to="/about-us">About us</CustomLink>
             </li>
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Careers</CustomLink>
+              <CustomLink to="/work-with-us">Careers</CustomLink>
             </li>
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Reviews</CustomLink>
+              <CustomLink to="/reviews">Reviews</CustomLink>
             </li>
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Showrooms</CustomLink>
+              <CustomLink to="/our-showrooms">Showrooms</CustomLink>
             </li>
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Sustainability</CustomLink>
+              <CustomLink to="/why/impact/sustainability">Sustainability</CustomLink>
             </li>
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Warranties</CustomLink>
+              <CustomLink to="#">Warranties</CustomLink>
             </li>
           </ul>
 
@@ -91,10 +87,10 @@ const FooterMain = () => {
             <h2 className="mb-4 font-F37-light text-primary text-xl pt-20">Resources</h2>
             <ul className="font-extrabold pb-2 text-sm">
               <li className="text-xs leading-6 font-F37-light">
-                <CustomLink href="#">Downloads</CustomLink>
+                <CustomLink to="/resources#brochures-pricing-guide">Downloads</CustomLink>
               </li>
               <li className="text-xs leading-6 font-F37-light">
-                <CustomLink href="#">Portal</CustomLink>
+                <CustomLink to="#">Portal</CustomLink>
               </li>
             </ul>
           </div>
@@ -105,16 +101,16 @@ const FooterMain = () => {
           <h2 className="mb-4 font-F37-light text-primary text-xl">Start a project</h2>
           <ul className="font-extrabold pb-2 text-sm">
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Find an installer</CustomLink>
+              <CustomLink to="#">Find an installer</CustomLink>
             </li>
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Installation Guide</CustomLink>
+              <CustomLink to="#">Installation Guide</CustomLink>
             </li>
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Order a sample</CustomLink>
+              <CustomLink to="/order-sample">Order a sample</CustomLink>
             </li>
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Where to buy</CustomLink>
+              <CustomLink to="#">Where to buy</CustomLink>
             </li>
           </ul>
         </div>
@@ -124,29 +120,29 @@ const FooterMain = () => {
           <h2 className="mb-4 font-F37-light text-primary text-xl">Customer Service</h2>
           <ul className="font-extrabold pb-2 text-sm">
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Approved global distributors</CustomLink>
+              <CustomLink to="#">Approved global distributors</CustomLink>
             </li>
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">Contact us</CustomLink>
+              <CustomLink to="#">Contact us</CustomLink>
             </li>
             <li className="text-xs leading-6 font-F37-light">
-              <CustomLink href="#">FAQs</CustomLink>
+              <CustomLink to="#">FAQs</CustomLink>
             </li>
           </ul>
           <ul className="py-2 text-sm">
             General Enquiries
-            <li className="text-xs leading-8">
+            <li className="text-xs leading-8 hover:underline hover: decoration-green">
               <a href="tel:+4402074824661">020 7482 4661</a>
             </li>
-            <li className="text-xs leading-8">
+            <li className="text-xs leading-8 hover:underline hover: decoration-green">
               <a href="mailto:sales@thelivingoutdoors.com">sales@thelivingoutdoors.com</a>
             </li>
           </ul>
         </div>
-        
       </div>
       <StayinTheLoop />
     </div>
   );
 };
+
 export default FooterMain;
--- frontend/src/Components/Footer/Logos.jsx
+++ frontend/src/Components/Footer/Logos.jsx
@@ -4,28 +4,28 @@ const Logos = () => {
   return (
     <div className='flex justify-center items-center space-x-8 mt-2 mb-6'>
       {/* Facebook SVG */}
-      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className='h-14 w-14 flex justify-center items-center no-underline'>
+      <a href="https://www.facebook.com/MillboardFlooring/" target="_blank" rel="noopener noreferrer" className='h-14 w-14 flex justify-center items-center no-underline'>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 fill-current text-gray-700 hover:text-green" viewBox="0 0 24 24">
           <path d="M16.403,9H14V7c0-1.032,0.084-1.682,1.563-1.682h0.868c0.552,0,1-0.448,1-1V3.064c0-0.523-0.401-0.97-0.923-1.005 C15.904,2.018,15.299,1.999,14.693,2C11.98,2,10,3.657,10,6.699V9H8c-0.552,0-1,0.448-1,1v2c0,0.552,0.448,1,1,1l2-0.001V21 c0,0.552,0.448,1,1,1h2c0.552,0,1-0.448,1-1v-8.003l2.174-0.001c0.508,0,0.935-0.381,0.993-0.886l0.229-1.996 C17.465,9.521,17.001,9,16.403,9z"></path>
         </svg>
       </a>
       
       {/* X (Twitter) SVG */}
-      <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className='h-14 w-14 flex justify-center items-center no-underline'>
+      <a href="https://x.com/Millboard_UK" target="_blank" rel="noopener noreferrer" className='h-14 w-14 flex justify-center items-center no-underline'>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 fill-current text-gray-700 hover:text-green" viewBox="0 0 50 50">
           <path d="M 5.9199219 6 L 20.582031 27.375 L 6.2304688 44 L 9.4101562 44 L 21.986328 29.421875 L 31.986328 44 L 44 44 L 28.681641 21.669922 L 42.199219 6 L 39.029297 6 L 27.275391 19.617188 L 17.933594 6 L 5.9199219 6 z M 9.7167969 8 L 16.880859 8 L 40.203125 42 L 33.039062 42 L 9.7167969 8 z"></path>
         </svg>
       </a>
       
       {/* YouTube SVG */}
-      <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className="h-14 w-14 flex justify-center items-center no-underline">
+      <a href="https://www.youtube.com/channel/UCSvCnwAb4h1VdPVoFMKKRpw" target="_blank" rel="noopener noreferrer" className="h-14 w-14 flex justify-center items-center no-underline">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 fill-current text-gray-700 hover:text-green" viewBox="0 0 50 50">
       <path d="M 24.402344 9 C 17.800781 9 11.601563 9.5 8.300781 10.199219 C 6.101563 10.699219 4.199219 12.199219 3.800781 14.5 C 3.402344 16.898438 3 20.5 3 25 C 3 29.5 3.398438 33 3.898438 35.5 C 4.300781 37.699219 6.199219 39.300781 8.398438 39.800781 C 11.902344 40.5 17.898438 41 24.5 41 C 31.101563 41 37.097656 40.5 40.597656 39.800781 C 42.800781 39.300781 44.699219 37.800781 45.097656 35.5 C 45.5 33 46 29.402344 46.097656 24.902344 C 46.097656 20.402344 45.597656 16.800781 45.097656 14.300781 C 44.699219 12.101563 42.800781 10.5 40.597656 10 C 37.097656 9.5 31 9 24.402344 9 Z M 24.402344 11 C 31.601563 11 37.398438 11.597656 40.199219 12.097656 C 41.699219 12.5 42.898438 13.5 43.097656 14.800781 C 43.699219 18 44.097656 21.402344 44.097656 24.902344 C 44 29.199219 43.5 32.699219 43.097656 35.199219 C 42.800781 37.097656 40.800781 37.699219 40.199219 37.902344 C 36.597656 38.601563 30.597656 39.097656 24.597656 39.097656 C 18.597656 39.097656 12.5 38.699219 9 37.902344 C 7.5 37.5 6.300781 36.5 6.101563 35.199219 C 5.300781 32.398438 5 28.699219 5 25 C 5 20.398438 5.402344 17 5.800781 14.902344 C 6.101563 13 8.199219 12.398438 8.699219 12.199219 C 12 11.5 18.101563 11 24.402344 11 Z M 19 17 L 19 33 L 33 25 Z M 21 20.402344 L 29 25 L 21 29.597656 Z"></path>
     </svg>
       </a>
       
       {/* Instagram SVG */}
-      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className='h-14 w-14 flex justify-center items-center no-underline'>
+      <a href="https://www.instagram.com/living_outdoors.co.uk/" target="_blank" rel="noopener noreferrer" className='h-14 w-14 flex justify-center items-center no-underline'>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className="h-8 w-8 fill-current text-gray-700 hover:text-green">
           <path d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z M 37 11 A 2 2 0 0 0 35 13 A 2 2 0 0 0 37 15 A 2 2 0 0 0 39 13 A 2 2 0 0 0 37 11 z M 25 14 C 18.936712 14 14 18.936712 14 25 C 14 31.063288 18.936712 36 25 36 C 31.063288 36 36 31.063288 36 25 C 36 18.936712 31.063288 14 25 14 z M 25 16 C 29.982407 16 34 20.017593 34 25 C 34 29.982407 29.982407 34 25 34 C 20.017593 34 16 29.982407 16 25 C 16 20.017593 20.017593 16 25 16 z"></path>
         </svg>
